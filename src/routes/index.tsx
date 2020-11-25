7506d1c2517fb445110424193e26e75928145710
RumbleKAT <reki318@naver.com>
2020-11-25 23:58:05 +0900

    divide pages and router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import Home from './Home';
-import AddTodo from './AddTodo';
+import Home from '../pages/Home';
+import AddTodo from '../pages/AddTodo';
 import { Route, Redirect, BrowserRouter,Switch } from "react-router-dom";
 
 const Root : React.FC = () =>{
