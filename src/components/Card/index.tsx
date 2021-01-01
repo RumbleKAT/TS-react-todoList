0d4fa96e1a09a7a674d0b229d1cc2dcf8865f9a0
RumbleKAT <reki318@naver.com>
2021-01-01 22:42:53 +0900

    complete Card Component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -11,18 +11,21 @@ interface CardProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLLIE
 
 class Card extends React.Component<CardProps>{
     public render(){
-        const { title, name, id, type , ...liAttributes } = this.props; 
-
+        const { title, name, id, type, category } = this.props; 
+        //react 16버전 부터 data- prefix를 붙인 custom attribute 허용
         return(
             type === 'done' ?
-            <li className="content-element">
-                <h1>{title}</h1>
-                <p>check : ${id}</p>
+            <li className="content-element" id={id} data-type={type}>
+                <h2>{title}</h2>
+                <p>등록번호 : {id} 만든이 : {name}, 카테고리: {category}</p>
+                <button className='remove'>X</button>
+                <button className='next' id={id} data-type={type}>&rarr;</button>
             </li>
             : 
             <li className="content-element">
-                <h1>{title}</h1>
-                <p>등록번호 : ${id}</p>
+                <h2>{title}</h2>
+                <p>등록번호 : {id} 만든이 : {name}, 카테고리: {category} </p>
+                <button className='removeCard'>X</button>
             </li>
         )
     }
